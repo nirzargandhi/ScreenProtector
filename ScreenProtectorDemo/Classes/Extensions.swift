ff4668344086165fd727db9e736d5af103d40d01
nirzargandhi <54696036+nirzargandhi@users.noreply.github.com>
2025-05-15 21:45:42 +0530

    Code improvements

--- ScreenProtectorDemo/Classes/Extensions.swift
+++ ScreenProtectorDemo/Classes/Extensions.swift
@@ -40,49 +40,6 @@ extension UIViewController {
 }
 
 
-// MARK: -
-extension String {
-}
-
-
-// MARK: - UIColor
-extension UIColor {
-    
-    convenience init(hex: String, alpha: CGFloat = 1.0) {
-        var red: CGFloat = 0.0
-        var green: CGFloat = 0.0
-        var blue: CGFloat = 0.0
-        
-        var hexString = ""
-        
-        if hex.hasPrefix("#") {
-            let nsHex = hex as NSString
-            hexString = nsHex.substring(from: 1)
-        } else {
-            hexString = hex
-        }
-        
-        let scanner = Scanner(string: hexString)
-        var hexValue: CUnsignedLongLong = 0
-        if scanner.scanHexInt64(&hexValue) {
-            switch (hexString.count) {
-            case 3:
-                red = CGFloat((hexValue & 0xF00) >> 8) / 15.0
-                green = CGFloat((hexValue & 0x0F0) >> 4) / 15.0
-                blue = CGFloat(hexValue & 0x00F) / 15.0
-            case 6:
-                red = CGFloat((hexValue & 0xFF0000) >> 16) / 255.0
-                green = CGFloat((hexValue & 0x00FF00) >> 8) / 255.0
-                blue = CGFloat(hexValue & 0x0000FF) / 255.0
-            default:
-                debugPrint("Invalid HEX string, number of characters after '#' should be either 3, 6", terminator: "")
-            }
-        }
-        self.init(red:red, green:green, blue:blue, alpha:alpha)
-    }
-}
-
-
 // MARK: - UIButton
 extension UIButton {
     
